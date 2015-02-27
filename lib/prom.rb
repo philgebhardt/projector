3ce436a08d6483f04381f6ad44afdc1c40abfe95
vagrant <vagrant@localhost.localdomain>
2015-02-27 06:39:23 +0000

    created prom.rb

---
+++ lib/prom.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+
+def Project
+
+   # Creates a Project object with 'dir' as
+   # its root. If the parameter 'dir' is not
+   # passed to the function, the default is
+   # Dir.pwd (the present working directory)
+   def initialize( dir = Dir.pwd )
+   
+   end
+   
+end
