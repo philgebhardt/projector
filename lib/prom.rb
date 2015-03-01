4b67cd216681192ea5fb3e75c992e48c4cff4b01
unknown <gebhardt.phil@gmail.com>
2015-02-28 23:20:27 -0800

    Added more of base object model to prom.rb

--- lib/prom.rb
+++ lib/prom.rb
@@ -1,9 +1,100 @@
 #!/bin/ruby
 
+class ProjectEntity
+
+   @name
+   @location
+   @parent
+   
+   def getName()
+      @name
+   end
+   
+   def setName(name)
+      @name = name
+   end
+   
+   def getLocation()
+      @location
+   end
+   
+   def setLocation(location)
+      @location = location
+   end
+   
+   def getParent()
+      @parent
+   end
+   
+   def setParent(projectDir)
+      @parent = projectDir
+   end
+   
+end
+
+class ProjectFile > ProjectEntity
+
+   def getContent()
+      content = nil
+      File.open(@location, 'r') do |f|
+	    content = f.read
+	  end
+	  content
+   end
+   
+   def setContent(content)
+      File.open(@location, 'w') do |f|
+	    f.write content
+	  end
+   end
+   
+   def isDirectory()
+      false
+   end
+   
+end 
+
+class ProjectDirectory > ProjectEntity
+
+   @children
+   
+   def getChildren()
+      unless @children
+	    @children = []
+	  end
+	  @children
+   end
+   
+   def setChildren(children)
+      @children = children
+   end
+   
+   def getChildrenMap()
+       map = {}
+	   dirs = []
+	   files = []
+	   @children.each do |child|
+	      if child.isDirectory()
+		     dirs << child
+		  else
+		     files << child
+		  end
+	   end
+	   map[:dirs] = dirs
+	   map[:files] = files
+   end
+   
+   def isDirectory()
+      true
+   end
+   
+end
+
 class Project
 
    # The root directory of the project
-   @root_dir
+   @rootDir
+   
    # Project name
    @name
    
@@ -12,18 +103,23 @@ class Project
    # its root. If the parameter 'dir' is not
    # passed to the function, the default is
    # Dir.pwd (the present working directory)
-   def initialize( dir = Dir.pwd )
-      @root_dir = dir
+   def initialize( dir )
+      @rootDir = dir
    end
    
-   def map(from_dir = @root_dir)
-      map = {}
-	  Dir.entries(from_dir).each do |entry|
-	    if File.directory?(entry)
-			map[entry] = map(entry)
-		else
-		    map[entry] = File.extname(entry)
-		end
-	  end
+   def getRootDir()
+      @rootDir
    end
+   
+   def setRootDir(dir)
+      @rootDir = dir
+   end
+   
+end
+
+class ProjectBuilder
+
+	@options=nil
+	@rootDir
+	
 end
